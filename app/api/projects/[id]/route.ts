f4efd3744c6477117f83e6028d2defba07e3265c
emmanuelandyeze <emmanuel.andyeze@gmail.com>
2025-08-10 21:50:31 +0100

    update

--- app/api/projects/[id]/route.ts
+++ app/api/projects/[id]/route.ts
@@ -19,7 +19,7 @@ export async function GET(
 	request: Request,
     { params }: { params: Promise<{ id: string }> },
 ): Promise<NextResponse<any>> {
-	const { id } = params;
+	const { id } = await params;
 
 	await connect();
 
